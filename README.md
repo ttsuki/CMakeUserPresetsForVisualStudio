db3321a819c9ec6e257e93e83ecc6020931477c4
ttsuki <tu3+github@tu3.jp>
2021-11-01 05:59:13 +0900

    Add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# CMakeUserPresets.json for Visual Studio 2019/2022
+
+I have wanted to switch platform/compier/architecture/configurations on Visual Studio IDE.
---
+++ Test.cpp
@@ -0,0 +1,44 @@
+﻿#include <string>
+#include <iostream>
+
+int main()
+{
+	// To check for compiler output and intellisense.
+	using namespace std;
+
+	cout << "Hello CMake." << endl;
+
+#if defined(__GNUC__)
+	cout << "__GNUC__ IS DEFINED." << endl;
+#endif
+
+#if defined(__clang__)
+	cout << "__clang__ IS DEFINED." << endl;
+#endif
+
+#if defined(_MSC_VER)
+	cout << "_MSC_VER IS DEFINED." << endl;
+#endif
+
+#if defined(NDEBUG)
+	cout << "NDEBUG IS DEFINED." << endl;
+#endif
+
+#if defined(__OPTIMIZE__)
+	cout << "__OPTIMIZE__ IS DEFINED" << endl;
+#endif
+
+	cout << "sizeof long: " << to_string(sizeof(long)) << endl;
+	cout << "sizeof int: " << to_string(sizeof(int)) << endl;
+	cout << "sizeof void*: " << to_string(sizeof(void*)) << endl;
+
+#if defined(_M_X64)
+	cout << "_M_X64 IS DEFINED." << endl;
+#endif
+
+#if defined(__x86_64__)
+	cout << "__x86_64__ IS DEFINED." << endl;
+#endif
+
+	return 0;
+}
